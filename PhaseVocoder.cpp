95ba72f3214f7c71a88b8b5020d0b3922546f0e3
bautolp <32584156+bautolp@users.noreply.github.com>
2018-11-05 13:40:55 -0800

    Added function templates
    Added function templates

--- PhaseVocoder.cpp
+++ PhaseVocoder.cpp
@@ -1,9 +1,8 @@
 #include "PhaseVocoder.h"
 
-PhaseVocoder::PhaseVocoder(uint32_t window_size, uint32_t hop_size)
+PhaseVocoder::PhaseVocoder(uint32_t window_size, uint32_t hop_size, WindowFunctionType window_type)
     : m_window_size(window_size), m_hop_size(hop_size), m_forward_fft(NULL), m_reverse_fft(NULL), m_window_buffer(NULL)
 {
-    //while(m_window_size >> 2)
     int order = -1;
     for (int i = 0; i < 32; i++)
     {
@@ -14,6 +13,24 @@ PhaseVocoder::PhaseVocoder(uint32_t window_size, uint32_t hop_size)
     m_forward_fft = new dsp::FFT(order);
     m_reverse_fft = new dsp::FFT(order);
     m_window_buffer = new float[m_window_size];
+    m_window_function = new float[m_window_size];
+    GenerateWindowFunction(window_type);
+}
+
+void PhaseVocoder::GenerateWindowFunction(WindowFunctionType window_type)
+{
+    switch (window_type)
+    {
+    case WindowFunctionType::Flat:
+        for (uint32_t curr_pos = 0; curr_pos < m_window_size; curr_pos++)
+        {
+            m_window_function[curr_pos] = (m_hop_size / m_window_size);
+        }
+        break;
+    case WindowFunctionType::Hanning:
+
+        break;
+    }
 }
 
 PhaseVocoder::~PhaseVocoder()
@@ -24,6 +41,28 @@ PhaseVocoder::~PhaseVocoder()
         delete m_reverse_fft;
     if (m_window_buffer)
         delete[] m_window_buffer;
+    if (m_window_function)
+        delete[] m_window_function;
+}
+
+void PhaseVocoder::ApplyWindowFunction(float* buffer)
+{
+
+}
+
+void PhaseVocoder::ApplyProcessing()
+{
+
+}
+
+void PhaseVocoder::PhaseLock()
+{
+
+}
+
+void PhaseVocoder::ReScale(float* buffer, float* scale)
+{
+
 }
 
 void PhaseVocoder::DSPProcessing(float* input, float* output, uint32_t hop_size, uint32_t buff_size)
--- PhaseVocoder.h
+++ PhaseVocoder.h
@@ -12,17 +12,31 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+enum WindowFunctionType
+{
+    Flat,
+    Hanning
+};
+
 class PhaseVocoder
 {
 public:
-    PhaseVocoder(uint32_t window_size, uint32_t hop_size);
+    PhaseVocoder(uint32_t window_size, uint32_t hop_size, WindowFunctionType window_type);
     ~PhaseVocoder();
+    void DSPProcessing(float* input, float* output, uint32_t hop_size, uint32_t buff_size);
 
 private:
-    void  DSPProcessing(float* input, float* output, uint32_t hop_size, uint32_t buff_size);
+
+    void GenerateWindowFunction(WindowFunctionType window_type);
+    void ApplyWindowFunction(float* buffer);
+    void ApplyProcessing();
+    void PhaseLock();
+    void ReScale(float* buffer, float* scale);
+
     uint32_t m_window_size;
     uint32_t m_hop_size;
     dsp::FFT *m_forward_fft;
     dsp::FFT *m_reverse_fft;
     float *m_window_buffer;
+    float *m_window_function;
 };
